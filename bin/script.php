b1e40bb9f3cd67410fd33ab48e6da9858878c063
Jonas Rudlang <jonas.rudlang@gmail.com>
2014-08-18 22:00:23 +0200

    Added ace and packager

--- bin/script.php
+++ bin/script.php
@@ -1,50 +1,84 @@
-Derp
 <?php
-	$arch_dir 		= "archive/";
-	$pkg_dir 			= "packages/";
-	$pkg_listfile = "packlist.json";
-	$pkg_infofile = "package.json";
+$arch_dir 		= "archive/";
+$pkg_dir 			= "packages/";
+$pkg_listfile = "packlist.json";
+$pkg_infofile = "package.json";
 
-	$packlist = json_decode(
-							file_get_contents($pkg_listfile), true);
+$option_list = array(
+	"force:",
+	"remove:",
+	"rebuild"
+);
 
-	function scandir_ext($dir) {
-		return array_diff(
-						scandir($dir),
-						array("..","."));
+$opt = getopt("", $option_list);
+
+$packlist = json_decode(
+						file_get_contents($pkg_listfile), true);
+
+function scandir_ext($dir) {
+	return array_diff(
+					scandir($dir),
+					array("..","."));
+}
+
+$stability = scandir_ext($pkg_dir);
+
+foreach ($stability as $stb) {
+	$archive = $arch_dir.$stb."/";
+	$method="";
+
+	if (isset($opt["force"]))
+		$method = "force";
+
+	if (isset($opt["remove"])) {
+		$method = "remove";
+		$delete = true;
 	}
 
-	$stability = scandir_ext($pkg_dir);
+	if (isset($opt[$method])) {
+			$name = (is_array($opt[$method])?
+				$opt[$method][0]:$opt[$method]);
 
-	foreach ($stability as $stb) {
-
-		$packs = scandir_ext($pkg_dir.$stb);
-
-		foreach ($packs as $pkg) {
-			$subdir = $stb."/".$pkg."/";
-			if (!isset($packlist[$stb][$pkg])) {
-
-				// Get package information
-				$pkginfo = json_decode(
-										file_get_contents(
-										$pkg_dir.$subdir.$pkg_infofile),
-										true);
-
-				$archive = $arch_dir.$stb."/";
-
-				// Create archive and compress it using GZip
-				$phar = new PharData($archive.$pkg.".tar");
-				$phar->buildFromDirectory($pkg_dir.$subdir."root/");
-				$phar->compress(Phar::GZ);
-
-				// Add version later bacause "compress" is buggy
-				rename($archive.$pkg.".tar.gz",$archive.$pkg.
-								"-".$pkginfo["version"].".tar.gz");
-
-				unlink($archive.$pkg.".tar");
-
-				$packlist[$stb][$pkg] = $pkginfo;
+			if (isset($packlist[$stb][$name])) {
+				$tgz = glob($archive.$name."-".$packlist[$stb][$name]["version"]."*");
+				unset($packlist[$stb][$name]);
+				unlink($tgz[0]);
+				echo "$method: \"$name\".\n";
 			}
-		}
-		//print_r($packs);
 	}
+
+	$packs = scandir_ext($pkg_dir.$stb);
+
+	foreach ($packs as $pkg) {
+		$subdir = $stb."/".$pkg."/";
+		if (!isset($packlist[$stb][$pkg]) && (!isset($delete))) {
+
+			// Get package information
+			$pkginfo = json_decode(
+									file_get_contents(
+									$pkg_dir.$subdir.$pkg_infofile),
+									true);
+
+			// Create archive and compress it using GZip
+			$phar = new PharData($archive.$pkg.".tar");
+			$phar->buildFromDirectory($pkg_dir.$subdir."root/");
+			$phar->compress(Phar::GZ);
+
+			$newname = $archive.$pkg.
+							"-".$pkginfo["version"].".tar.gz";
+
+			// Add version later bacause "compress" is buggy
+			rename($archive.$pkg.".tar.gz",$newname);
+
+			echo "Packed ".$newname."\n";
+
+			unlink($archive.$pkg.".tar");
+
+			$packlist[$stb][$pkg] = $pkginfo;
+		}
+	}
+
+}
+
+file_put_contents($pkg_listfile,
+	json_encode($packlist, JSON_PRETTY_PRINT));
